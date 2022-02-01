6768f9e386e41ca2a448c02364ce0c5141a504c0
Eva <Eva@DESKTOP-EBTPU5S>
2022-02-01 20:34:44 +0200

    Add project files.

---
+++ DoctorAppointments/Controllers/Doctors1Controller.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoctorAppointments.Models;
+
+namespace DoctorAppointments.Controllers
+{
+    public class Doctors1Controller : Controller
+    {
+        private AppointmentsEntities db = new AppointmentsEntities();
+
+        [HttpPost]
+        public ActionResult DoctorRegister(Doctor doctor)
+        {
+            if (doctor == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            db.Doctors.Add(doctor);
+            db.SaveChanges();
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+            //return View(appointment)
+        }
+
+
+        // GET: Doctors1
+        //public ActionResult Index()
+        //{
+        //    return View(db.Doctors.ToList());
+        //}
+
+        //// GET: Doctors1/Details/5
+        //public ActionResult Details(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Doctor doctor = db.Doctors.Find(id);
+        //    if (doctor == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(doctor);
+        //}
+
+        //// GET: Doctors1/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Doctors1/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "doctorAMKA,doctorID,password,name,surname,username,speciality")] Doctor doctor)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Doctors.Add(doctor);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(doctor);
+        //}
+
+        //// GET: Doctors1/Edit/5
+        //public ActionResult Edit(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Doctor doctor = db.Doctors.Find(id);
+        //    if (doctor == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(doctor);
+        //}
+
+        //// POST: Doctors1/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "doctorAMKA,doctorID,password,name,surname,username,speciality")] Doctor doctor)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(doctor).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(doctor);
+        //}
+
+        //// GET: Doctors1/Delete/5
+        //public ActionResult Delete(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Doctor doctor = db.Doctors.Find(id);
+        //    if (doctor == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(doctor);
+        //}
+
+        //// POST: Doctors1/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(long id)
+        //{
+        //    Doctor doctor = db.Doctors.Find(id);
+        //    db.Doctors.Remove(doctor);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
---
+++ DoctorAppointments/Controllers/PatientsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoctorAppointments.Models;
+
+namespace DoctorAppointments.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private AppointmentsEntities db = new AppointmentsEntities();
+
+
+        [HttpGet]
+        public ActionResult Login(string username, string password, string role)
+        {
+            if (username == null || password == null || role == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (role.Contains("Patient"))
+            {
+                Patient user = db.Patients.Where(x => x.username == username).FirstOrDefault();
+                if (user == null) return HttpNotFound();
+                return View(user);
+            }
+            else if (role.Contains("Doctor"))
+            {
+                Doctor user = db.Doctors.Where(x => x.username == username).FirstOrDefault();
+                if (user == null) return HttpNotFound();
+                return View(user);
+            }
+            else
+            {
+                Admin user = db.Admins.Where(x => x.username == username).FirstOrDefault();
+                if (user == null) return HttpNotFound();
+                return View(user);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult PatientRegister(Patient patient)
+        {
+            if (patient == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            db.Patients.Add(patient);
+            db.SaveChanges();
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+            //return View(patient)
+        }
+
+        // GET: Patients
+        //public ActionResult Index()
+        //{
+        //    return View(db.Patients.ToList());
+        //}
+
+        //// GET: Patients/Details/5
+        //public ActionResult Details(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Patient patient = db.Patients.Find(id);
+        //    if (patient == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(patient);
+        //}
+
+        //// GET: Patients/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Patients/Create
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "patientAMKA,patientID,password,name,surname,username")] Patient patient)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Patients.Add(patient);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(patient);
+        //}
+
+        //// GET: Patients/Edit/5
+        //public ActionResult Edit(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Patient patient = db.Patients.Find(id);
+        //    if (patient == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(patient);
+        //}
+
+        //// POST: Patients/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "patientAMKA,patientID,password,name,surname,username")] Patient patient)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(patient).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(patient);
+        //}
+
+        //// GET: Patients/Delete/5
+        //public ActionResult Delete(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Patient patient = db.Patients.Find(id);
+        //    if (patient == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(patient);
+        //}
+
+        //// POST: Patients/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(long id)
+        //{
+        //    Patient patient = db.Patients.Find(id);
+        //    db.Patients.Remove(patient);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
