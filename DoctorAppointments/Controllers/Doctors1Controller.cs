275408f4888819d7b96c0be11c5dd3fbff03a6d9
Eva <Eva@DESKTOP-EBTPU5S>
2022-02-02 20:19:28 +0200

    new controllers

--- DoctorAppointments/Controllers/Doctors1Controller.cs
+++ DoctorAppointments/Controllers/Doctors1Controller.cs
@@ -34,114 +34,32 @@ namespace DoctorAppointments.Controllers
             return View(doctors);
         }
 
-        // GET: Doctors1
-        //public ActionResult Index()
+        [HttpGet]
+        public ActionResult ShowAppointmentsPerDay(long doctorAMKA)
+        {
+            DateTime currentDate = DateTime.Now;
+            
+            List<Appointment> currentAppointments = db.Appointments.Where(x => x.doctorAMKA == doctorAMKA )
+                                                    .Where( w => w.appointmentDate == currentDate.Date )
+                                                    .Where( y => y.isAvailable == false)
+                                                    .ToList();
+            return View(currentAppointments);
+        }
+
+
+        //Unfinished
+        //[HttpGet]
+        //public ActionResult ShowAppointmentsPerWeek(long doctorAMKA)
         //{
-        //    return View(db.Doctors.ToList());
+        //    DateTime currentDate = DateTime.Now;
+
+        //    List<Appointment> currentAppointments = db.Appointments.Where(x => x.doctorAMKA == doctorAMKA)
+        //                                            .Where(w => w.appointmentDate == currentDate.Date)
+        //                                            .Where(y => y.isAvailable == false)
+        //                                            .ToList();
+        //    return View(currentAppointments);
         //}
 
-        //// GET: Doctors1/Details/5
-        //public ActionResult Details(long? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Doctor doctor = db.Doctors.Find(id);
-        //    if (doctor == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(doctor);
-        //}
-
-        //// GET: Doctors1/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Doctors1/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "doctorAMKA,doctorID,password,name,surname,username,speciality")] Doctor doctor)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Doctors.Add(doctor);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    return View(doctor);
-        //}
-
-        //// GET: Doctors1/Edit/5
-        //public ActionResult Edit(long? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Doctor doctor = db.Doctors.Find(id);
-        //    if (doctor == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(doctor);
-        //}
-
-        //// POST: Doctors1/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "doctorAMKA,doctorID,password,name,surname,username,speciality")] Doctor doctor)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(doctor).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(doctor);
-        //}
-
-        //// GET: Doctors1/Delete/5
-        //public ActionResult Delete(long? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Doctor doctor = db.Doctors.Find(id);
-        //    if (doctor == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(doctor);
-        //}
-
-        //// POST: Doctors1/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(long id)
-        //{
-        //    Doctor doctor = db.Doctors.Find(id);
-        //    db.Doctors.Remove(doctor);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
+        
     }
 }
